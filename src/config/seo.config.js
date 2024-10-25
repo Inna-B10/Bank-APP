e924dbeb56605b57b4cc8c9ca0d31483e320a07a
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-10-25 14:41:46 +0200

    RenderService: #htmlToElement, #replaceComponentTags

--- src/config/seo.config.js
+++ src/config/seo.config.js
@@ -1,5 +1,5 @@
 const SITE_NAME = 'Bank App - Vanilla JS'
 
 export const getTitle = title => {
-	return title ? `${title} | {SITE_NAME}` : SITE_NAME
+	return title ? `${title} | ${SITE_NAME}` : SITE_NAME
 }
