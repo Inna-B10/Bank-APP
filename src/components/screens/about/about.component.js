c09e6da752e863a9be667548028d27c56a9c92a3
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-10-25 13:24:43 +0200

    component BaseScreen, dynamic page title

--- src/components/screens/about/about.component.js
+++ src/components/screens/about/about.component.js
@@ -1,4 +1,9 @@
-export class About {
+import { BaseScreen } from '@/core/component/base-screen.component'
+
+export class About extends BaseScreen {
+	constructor() {
+		super({ title: 'About' })
+	}
 	render() {
 		return '<p>About</p>'
 	}
