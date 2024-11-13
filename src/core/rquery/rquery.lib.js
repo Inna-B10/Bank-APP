209963cc91ac0a2a73dba43f2a599f2cc31b43a1
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-11-13 13:53:26 +0100

    new methods in RQuery for form (inputs)

--- src/core/rquery/rquery.lib.js
+++ src/core/rquery/rquery.lib.js
@@ -1,8 +1,8 @@
-// $R('#rrt').find('.rge').css('', '').text('').html('')
+import { formatCardNumberWithDashes } from '@/utils/format/format-card-number'
 
-/**
- * Represents the RQuery class for working with DOM elements.
- */
+/* ========================================================================== */
+/*         REPRESENTS THE RQUERY CLASS FOR WORKING WITH DOM ELEMENTS.         */
+/* ========================================================================== */
 
 /**
  * Create a new RQuery instance.
@@ -22,12 +22,13 @@ class RQuery {
 			throw new Error('Invalid selector type')
 		}
 	}
-
+	/* ---------------------------------- Find ---------------------------------- */
 	/**
 	 * Find the first element that matches the specified selector within the selected element.
 	 * @param {string} selector - A CSS selector string to search for within the selected element.
 	 * @returns {RQuery} A new RQuery instance for the found element.
 	 */
+
 	find(selector) {
 		const element = new RQuery(this.element.querySelector(selector))
 		if (element) {
@@ -36,22 +37,24 @@ class RQuery {
 			throw new Error(`Element ${selector} not found!`)
 		}
 	}
-
+	/* --------------------------------- Append --------------------------------- */
 	/**
 	 * Append a new element as a child of the selected element.
 	 * @param {HTMLElement} childElement - A new child element to append.
 	 * @returns {RQuery} The current RQuery instance for chaining.
 	 */
+
 	append(childElement) {
 		this.element.appendChild(childElement)
 		return this
 	}
-
+	/* --------------------------------- Before --------------------------------- */
 	/**
 	 * Insert a new element before the selected element.
 	 * @param {HTMLElement} - A new element to insert before the selected element.
 	 * @returns {RQuery} The current RQuery instance for chaining.
 	 */
+
 	before(newElement) {
 		if (!(newElement instanceof HTMLElement)) {
 			throw new Error('Element must be a HTMLElement')
@@ -64,12 +67,13 @@ class RQuery {
 			throw new Error('Element does not have a parent element')
 		}
 	}
-
+	/* ---------------------------------- Html ---------------------------------- */
 	/**
 	 * Get or set the inner HTML of the selected element.
 	 * @param {string} [htmlContent] - OptionalHTML content to set. Id not provided,the current inner HTML will be returned.
 	 * @returns {RQuery|string} The current RQuery instance for chaining when setting HTML content, or the current inner HTML when getting.
 	 */
+
 	html(htmlContent) {
 		if (typeof htmlContent === 'undefined') {
 			return this.element.innerHTML
@@ -78,23 +82,96 @@ class RQuery {
 			return this
 		}
 	}
-
+	/* ---------------------------------- Click --------------------------------- */
 	/**
 	 * Attach a click event listener to the selected element.
-	 * @param {function(Event): void} - The event listener function to execute when the selected element is clicked. The function will receive the event object as its argument.
+	 * @param {function(Event): void} - The event listener function to execute when the selected element is clicked.
+	 * The function will receive the event object as its argument.
 	 * @returns {RQuery} The current RQuery instance for chaining.
 	 */
+
 	click(callback) {
 		this.element.addEventListener('click', callback)
 		return this
 	}
+	/* ---------------------------------- Input --------------------------------- */
+	/**
+	 * Set attributes and event listeners for an input element.
+	 * @param {object} options - An object containing input options
+	 * @param {function(Event): void} [options.onInput] - The event listener for the input's input event
+	 * @param {object} [options.rest] - Optional attributes to set on the input element
+	 * @returns {RQuery} The current RQuery instance for chaining
+	 */
 
+	input({ onInput, ...rest }) {
+		if (this.element.tagName.toLowerCase() !== 'input') {
+			throw new Error('Element must be an input')
+		}
+
+		for (const [key, value] of Object.entries(rest)) {
+			this.element.setAttribute(key, value)
+		}
+
+		if (onInput) {
+			this.element.addEventListener('input', onInput)
+		}
+		return this
+	}
+	/* ------------------------------- NumberInput ------------------------------ */
+	/**
+	 * Set attributes and event listeners for a number input element
+	 * @param {number} [limit] - The maximum length of input value
+	 * @returns {RQuery} The current RQuery instance for chaining
+	 */
+
+	numberInput(limit) {
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'number'
+		) {
+			throw new Error('Element must be an input with type "number"')
+		}
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) {
+				value = value.substring(0, limit)
+			}
+			event.target.value = value
+		})
+		return this
+	}
+	/* ----------------------------- CreditCardInput ---------------------------- */
+	/**
+	 * Set attributes and event listeners for a credit card input element
+	 * @returns {RQuery} The current RQuery instance for chaining
+	 */
+
+	creditCardInput() {
+		const limit = 16
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'text'
+		) {
+			throw new Error('Element must be an input with type "text"')
+		}
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			value = value.substring(0, limit)
+			event.target.value = formatCardNumberWithDashes(value)
+		})
+		return this
+	}
+
+	/* ----------------------------------- Css ---------------------------------- */
 	/**
 	 * Set the CSS style of the selected element.
 	 * @param {string} property - The CSS property to set.
 	 * @param {string} value - The value to set for the CSS property.
 	 * @returns {RQuery} The current RQuery instance for chaining.
 	 */
+
 	css(property, value) {
 		if (typeof property !== 'string' || typeof value !== 'string') {
 			throw new Error('Property and value must be strings')
@@ -102,13 +179,14 @@ class RQuery {
 		this.element.style[property] = value
 		return this
 	}
-
+	/* -------------------------------- AddClass -------------------------------- */
 	/**
 	 *
 	 * Adds a class or a list of classes to the current element.
 	 * @param {string | string[]} classNames - A single class name or an array of class names to add to the element.
 	 * @returns {RQuery} The current RQuery instance for chaining.
 	 */
+
 	addClass(classNames) {
 		if (Array.isArray(classNames)) {
 			for (const className of classNames) {
@@ -119,13 +197,14 @@ class RQuery {
 		}
 		return this
 	}
-
+	/* ------------------------------- RemoveClass ------------------------------ */
 	/**
 	 *
 	 * Removes a class or a list of classes from the current element.
 	 * @param {string | string[]} classNames - A single class name or an array of class names to remove from the element.
 	 * @returns {RQuery} The current RQuery instance for chaining.
 	 */
+
 	removeClass(classNames) {
 		if (Array.isArray(classNames)) {
 			for (const className of classNames) {
