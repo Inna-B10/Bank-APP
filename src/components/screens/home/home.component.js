178f8462de2b56052f4930096322168427710f13
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-11-13 12:39:39 +0100

    test Button

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,6 +1,7 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
 import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
+import { Button } from '@/components/ui/button/button.component'
 import styles from './home.module.scss'
 import template from './home.template.html'
 
@@ -9,7 +10,17 @@ export class Home extends BaseScreen {
 		super({ title: 'Home' })
 	}
 	render() {
-		const element = renderService.htmlToElement(template, [], styles)
+		const element = renderService.htmlToElement(
+			template,
+			[
+				new Button({
+					children: 'Send',
+					onClick: () => alert('Hei'),
+					variant: 'green'
+				})
+			],
+			styles
+		)
 		$R(element).find('h1').css('color', 'blue')
 		return element
 	}
